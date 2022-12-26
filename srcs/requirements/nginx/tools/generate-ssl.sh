8779a33390e4c2469613910a0e54a36eb3cc8e9f
Bassam <bassam1881999@gmail.com>
2022-12-26 15:34:18 +0400

    successfully connect nginx with wordpress container with proxy_pass and adding  to /etc/hosts

--- srcs/requirements/nginx/tools/generate-ssl.sh
+++ srcs/requirements/nginx/tools/generate-ssl.sh
@@ -4,16 +4,16 @@ echo
 echo -e "\033[32m------------------ ssl files are successfully CREATED ------------------\033[0m"
 
 # create ssl directory
-SSL_DIR="../ssl"
+SSL_DIR="../certs"
 mkdir -p ${SSL_DIR}
 
 # mv ssl files
-mv certificate.pem SSL/certificate.pem
+mv certificate.pem ${SSL_dir}/certificate.pem
 if [ $? -ne 0 ]; then
   echo -e "\033[31m------------------ ssl files are NOT MOVED ------------------\033[0m"
   exit 1
 fi
-mv key.pem SSL/key.pem
+mv key.pem ${SSL_dir}/key.pem
 if [ $? -ne 0 ]; then
   echo -e "\033[31m------------------ ssl files are NOT MOVED ------------------\033[0m"
   exit 1
